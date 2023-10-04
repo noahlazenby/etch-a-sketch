b1c0d6b092b6d199eeac7f31bbbd3e6786de48a1
Noah Lazenby <noahlazenby19@gmail.com>
2023-10-03 18:57:06 -0700

    added change dimensions button

--- myscript.js
+++ myscript.js
@@ -1,6 +1,15 @@
 //create a 16x16 grid of square divs 
 const container = document.querySelector('#container');
 
+function applyHoverOn () {
+    let grid = document.querySelectorAll('div.gridItem');
+    grid.forEach((square) => {
+        square.addEventListener("mouseenter", () => {
+            square.classList.toggle('hoverOn');
+        });   
+     });
+};
+
 function makeRows(rows, cols) {
     container.style.setProperty('--gridRows', rows);
     container.style.setProperty('--gridCols', cols);
@@ -8,20 +17,19 @@ function makeRows(rows, cols) {
       let cell = document.createElement("div");
       container.appendChild(cell).className = "gridItem";
     };
+    applyHoverOn();
   };
   
+  const changeButton = document.querySelector('#userInput');
+  changeButton.addEventListener("click", () => {
+    let input = prompt("Enter number of squares per side");
+    makeRows(input, input);
+    grid = document.querySelectorAll('div.gridItem');
+  })
+ 
+  makeRows(16,16);
+ 
+  
+
   
  
-  makeRows(16, 16);
-  const grid = document.querySelectorAll('div.gridItem');
-  grid.forEach((square) => {
-    square.addEventListener("mouseenter", () => {
-        square.classList.add('hoverOn');
-        });  
-  });
-  /*grid.addEventListener("mouseenter", () => {
-    grid.forEach(item => {
-        item.classList.add('.hoverOn');
-    });
-  });*/
-  console.log(grid);
