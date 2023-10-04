3a9bad5fc28fd5a012a611187836ec8929a37d8d
Noah Lazenby <noahlazenby19@gmail.com>
2023-10-03 18:38:13 -0700

    change background color on mouse enter

--- myscript.js
+++ myscript.js
@@ -10,8 +10,18 @@ function makeRows(rows, cols) {
     };
   };
   
-  const grid = document.querySelectorAll('div.gridItem');
-  grid.addEventListener("hover", () => {
-    
-  })
+  
+ 
   makeRows(16, 16);
+  const grid = document.querySelectorAll('div.gridItem');
+  grid.forEach((square) => {
+    square.addEventListener("mouseenter", () => {
+        square.classList.add('hoverOn');
+        });  
+  });
+  /*grid.addEventListener("mouseenter", () => {
+    grid.forEach(item => {
+        item.classList.add('.hoverOn');
+    });
+  });*/
+  console.log(grid);
